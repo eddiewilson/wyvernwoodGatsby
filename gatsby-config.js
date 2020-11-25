ab8648ec5f9775c61371bb0569dc4b6f7ae75991
Ed Wilson <ed@gendall.co.uk>
2020-11-25 11:31:44 +0000

    added some meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Wyvernwood - Another World Awaits`,
+    description: `Welcome to Wyvernwood – a magical kingdom where fairies, dragons and goblins thrive. Explore the goblins’ tunnels, scale the castle walls and make wishes come true`,
+    author: `@gendalldesign`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -24,7 +24,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `static/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `static/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
     {
